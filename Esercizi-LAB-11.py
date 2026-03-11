d8fe31a127df2ca7eca928c72ecf7930de075cec
Vincenzo Riccotti <vinzert25@gmail.com>
2026-03-11 13:37:30 +0100

    Esercizi Lab 1

---
+++ Esercizi-LAB-11.py
@@ -0,0 +1,7 @@
+def converti(lista):
+    for elementi in lista:
+        print(my_diz[elementi])
+
+my_diz={0:"zero",1:"uno", 2:"due",3:"tre",4:"quattro",5:"cinque",6:"sei",7:"sette",8:"otto",9:"nove"}
+lista=[2,5,1]
+converti(lista)
