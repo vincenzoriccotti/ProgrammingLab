72663a07a406aefe2ae6417f9df18fd73e14d068
Vincenzo Riccotti <vinzert25@gmail.com>
2026-03-15 21:01:19 +0100

    Simulazione Lab1.3

--- Simulazione-2.py
+++ Simulazione-2.py
@@ -4,6 +4,11 @@
 class CSVTimeSeriesFile:
     def __init__(self,name):
         self.name_file=name
+        try:
+            with open(self.name_file,'r') as file:
+                pass
+        except Exception:
+            raise ExamException("Errore: impossibile aprire il file.")
     def get_data(self):
         
         with open(self.name_file,'r') as file:
@@ -17,10 +22,19 @@ class CSVTimeSeriesFile:
                 uncert=float(element[2])
                 if uncert <5:
                     lista.append([data,temp])
+                else:
+                    print("Data saltata perchè valore troppo incerto")
         return lista
     
+class ExamException(Exception):
+    pass
     
 def compute_month_variation(time_series,first_year,second_year):
+    if type(first_year) != int or type(second_year) != int:
+        raise ExamException("Errore: gli anni devono essere dit tipo intero.")
+    if first_year >= second_year:
+        raise ExamException("Errore: il secondo anno deve essere maggiore del primo.")
+    
     year_temp={}
     for element in time_series:
         date=element[0]
@@ -32,16 +46,21 @@ def compute_month_variation(time_series,first_year,second_year):
                 year_temp[anno]={}
             year_temp[anno][mese]=temp
     variations={}
+    count=0
     for month in range(1,13):
         if month in year_temp[first_year] and month in year_temp[second_year]:
             diff=year_temp[second_year][month] - year_temp[first_year][month]
             variations[month]=diff
-    
+        else:
+            print(f"La variazione per il mese {month} non calcolata perchè dati mancanti.")
+            count+=1
+    if count==12:
+        raise ExamException("Gli anni considerati non hanno mesi validi.")
     return variations
     
     
    
-time_series_file = CSVTimeSeriesFile(name="GlobalTemperatures.csv")
+time_series_file = CSVTimeSeriesFile(name="GloblTemperatures.csv")
 data = time_series_file.get_data()
 variazioni=compute_month_variation(data,1998,2001)
 print(variazioni)
